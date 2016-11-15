2ac12f94c7e5ec6b4c8c7300e40bbed1c5399c25
Viacheslav Slavinsky <svofski@gmail.com>
2016-11-15 20:07:40 +0300

    gendoc.sh

--- gendoc.sh
+++ gendoc.sh
@@ -3,3 +3,10 @@ gforth doc/gendoc.fs
 for f in *.fs ; do
   mv $f.html html/
 done
+git stash
+git checkout gh-pages
+cp -f html/* .
+git add *.html
+git commit -m gendoc
+git checkout master
+git stash pop
